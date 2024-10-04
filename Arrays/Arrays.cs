5581b5fadf74431939073b8916f24ce29b5d0234
Shubhameme <suratkarinbox@gmail.com>
2024-10-04 17:49:36 +0530

    Add project files.

---
+++ Arrays/Arrays.cs
@@ -0,0 +1,27 @@
+﻿namespace Arrays
+{
+    public class Arrays
+    {
+        public static void Main(string[] args)
+        {
+
+            int[,] mularray = new int[4, 2];
+            mularray[0, 0] = 11; mularray[0, 1] = 12;
+            mularray[1, 0] = 13; mularray[1, 1] = 14;
+            mularray[2, 0] = 15; mularray[2, 1] = 16;
+            mularray[3, 0] = 17; mularray[3, 1] = 18;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine(mularray[i, j]);
+
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Shubham.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Shubham
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 9, 5, 2, 1, 3, 4, 6, 8 };
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+            }
+
+           foreach(int item  in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        
+    }
+}
